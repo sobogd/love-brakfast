8025577f89ac3ae769400cf7b58ea22b7ab5d278
sobogd <sobogd@gmail.com>
2023-01-14 16:33:43 +0300

    fix hash + small imgs

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ ReactDOM.render(
       <Provider store={store}>
         <I18nextProvider i18n={i18n}>
           <Page />
+          {""}
         </I18nextProvider>
       </Provider>
     </BrowserRouter>
