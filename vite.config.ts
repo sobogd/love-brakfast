47d39916adb8a6221fbdb52cf23bb655b0bb444d
sobogd <sobogd@gmail.com>
2023-10-12 18:30:43 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "./build",
+    rollupOptions: {
+      output: {
+        // Prevent vendor.js being created
+        manualChunks: undefined,
+        // chunkFileNames: "zzz-[name].js",
+        // this got rid of the hash on style.css
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
   },
   server: {
     port: 4200,
