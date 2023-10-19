8abfc9002c35bc69c845444c81d852c68c61bb73
sobogd <sobogd@gmail.com>
2023-10-19 18:53:23 +0300

    new menu sliders

--- src/components/Positions.tsx
+++ src/components/Positions.tsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { useTranslation } from "react-i18next";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { categories, positions } from "../consts";
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -11,6 +19,9 @@ const Container = styled.div`
   overflow-x: hidden;
   flex-direction: column;
   align-items: flex-start;
+  * {
+    transition: none;
+  }
 `;
 
 const MenuTabs = styled.div`
@@ -36,257 +47,140 @@ const MenuTab = styled.span`
 `;
 
 const PositionsContainer = styled.div`
-  padding: 20px 15px;
   overflow-y: scroll;
+  overflow-x: hidden;
   width: 100%;
   min-height: 100%;
-  padding-bottom: 100px;
+  padding-bottom: 150px;
 `;
 
-const PositionCard = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 15px;
-  padding-bottom: 15px;
-  border-bottom: 1px solid #dddddd;
-  width: 100%;
-`;
-
-const PositionNumber = styled.div`
-  background: #ffffff8c;
-  border-radius: 30px;
-  min-width: 50px;
-  min-height: 50px;
+const SwiperCardNumber = styled.div`
+  top: 0;
+  left: 0;
+  width: 80px;
+  background: #ffffffd1;
+  font-weight: 600;
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-grow: 1;
-  font-weight: 600;
   ::before {
-    content: "#";
-    font-size: 10px;
-    margin-right: 2px;
+    content: "№";
   }
 `;
 
-const PositionName = styled.div`
-  margin: 0 20px;
-  text-align: center;
-  flex-grow: 1;
-  width: 100%;
-  p {
-    margin-top: 5px;
-    font-size: 12px;
-    color: #656565;
-  }
-`;
-
-const PositionPrice = styled.div`
-  font-weight: 600;
+const SwiperCardName = styled.div`
   font-size: 18px;
-  ::after {
-    content: "₺";
-    font-size: 14px;
-    margin-left: 2px;
+  font-weight: 600;
+  padding: 14px;
+  width: 100%;
+  text-align: center;
+  white-space: pre-wrap;
+  line-height: 22px;
+`;
+
+const SwiperCardPrice = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin: 2px 0 15px;
+`;
+
+const SwiperCardButton = styled.div`
+  background: #d7ccbd;
+  color: white;
+  padding: 8px 30px;
+  font-weight: 600;
+  margin-bottom: 5px;
+  margin-top: 6px;
+  :hover {
+    background: #c3b5a2;
   }
 `;
+
+const SwiperCardDescription = styled.div`
+  font-size: 14px;
+  padding: 0 20px 10px;
+  color: #797979;
+  width: 100%;
+  text-align: center;
+  margin-top: -4px;
+  line-height: 18px;
+  white-space: pre-wrap;
+`;
+
+const SwiperCardSwipeDescription = styled.div`
+  font-size: 10px;
+  text-align: center;
+  margin: -2px 0 10px;
+  color: gray;
+`;
+
+const SwiperCardImage = styled.img``;
 
 const Positions: React.FC = () => {
   const { t } = useTranslation();
-  // const location = useLocation();
-  // const dispatch = useAppDispatch();
-  // const isMenuOpened = useSelector(isMenuOpenedSelector);
-  // const { language } = i18n;
-  // const menu = useAppSelector((s) => s.menu);
 
   const [categoryId, setCategoryId] = useState<number | undefined>(1);
-
-  // React.useEffect(() => {
-  //   dispatch(menuSearch());
-  // }, []);
-
-  // React.useEffect(() => {
-  //   dispatch(closeMenus());
-  // }, [location, language]);
-
-  // const categories: { id: number; name: string }[] = useMemo(() => {
-  //   return (
-  //     menu?.map((category) => ({
-  //       id: category?.id || 0,
-  //       name: category?.name || "",
-  //     })) || []
-  //   );
-  // }, [menu]);
-
-  // const positions: { id: number; name: string; price: number }[] = useMemo(() => {
-  //   if (!categoryId || !menu) return [];
-  //   return (
-  //     menu
-  //       ?.find((c) => c.id === categoryId)
-  //       ?.items?.map((p) => ({
-  //         id: p.id || 0,
-  //         name: p?.t?.find((tr) => tr.code === i18n.language)?.title || p.name || "",
-  //         price: p.price ?? 0,
-  //       })) || []
-  //   );
-  // }, [menu, categoryId, i18n.language]);
-
-  // useEffect(() => {
-  //   setCategoryId(categories?.[0]?.id);
-  // }, [categories]);
 
   const handleClickCategory = (categoryId: number) => () => {
     setCategoryId(categoryId);
     document.getElementById("tabsForPositions")?.scrollTo({
       left: (document.getElementById("tabForCategory" + categoryId.toString())?.offsetLeft ?? 0) - 80,
     });
+    document.getElementById("tabForPositions")?.scrollTo({
+      top: 0,
+    });
   };
-
-  const positions = [
-    // breakfast
-    { number: 1, price: 155, descr: true, category: 1 },
-    { number: 2, price: 205, descr: true, category: 1 },
-    { number: 3, price: 80, descr: true, category: 1 },
-    { number: 4, price: 80, descr: true, category: 1 },
-    { number: 5, price: 80, descr: true, category: 1 },
-    { number: 6, price: 95, descr: true, category: 1 },
-    { number: 7, price: 155, descr: false, category: 1 },
-    { number: 8, price: 135, descr: false, category: 1 },
-    { number: 9, price: 135, descr: false, category: 1 },
-    { number: 10, price: 110, descr: true, category: 1 },
-    { number: 11, price: 110, descr: true, category: 1 },
-    { number: 12, price: 110, descr: true, category: 1 },
-    { number: 13, price: 110, descr: true, category: 1 },
-    { number: 14, price: 110, descr: true, category: 1 },
-    { number: 15, price: 110, descr: true, category: 1 },
-    { number: 16, price: 165, descr: true, category: 1 },
-    { number: 17, price: 165, descr: true, category: 1 },
-    { number: 18, price: 165, descr: true, category: 1 },
-    // toasts
-    { number: 19, price: 195, descr: false, category: 2 },
-    { number: 20, price: 125, descr: false, category: 2 },
-    { number: 21, price: 25, descr: true, category: 2 },
-    // soups
-    { number: 22, price: 95, descr: false, category: 3 },
-    { number: 23, price: 95, descr: false, category: 3 },
-    // sandviches
-    { number: 24, price: 165, descr: false, category: 4 },
-    { number: 25, price: 165, descr: false, category: 4 },
-    { number: 26, price: 145, descr: false, category: 4 },
-    // bowls
-    { number: 27, price: 220, descr: true, category: 5 },
-    { number: 28, price: 220, descr: true, category: 5 },
-    { number: 29, price: 185, descr: true, category: 5 },
-    { number: 30, price: 165, descr: true, category: 5 },
-    // salats
-    { number: 31, price: 95, descr: true, category: 6 },
-    { number: 32, price: 165, descr: true, category: 6 },
-    // hot drinks
-    { number: 33, price: 55, descr: false, category: 7 },
-    { number: 34, price: 70, descr: false, category: 7 },
-    { number: 35, price: 40, descr: false, category: 7 },
-    { number: 36, price: 60, descr: false, category: 7 },
-    { number: 37, price: 50, descr: false, category: 7 },
-    { number: 38, price: 70, descr: false, category: 7 },
-    { number: 39, price: 65, descr: false, category: 7 },
-    { number: 40, price: 85, descr: false, category: 7 },
-    { number: 41, price: 85, descr: false, category: 7 },
-    { number: 42, price: 65, descr: false, category: 7 },
-    { number: 43, price: 70, descr: false, category: 7 },
-    { number: 44, price: 55, descr: false, category: 7 },
-    { number: 45, price: 30, descr: false, category: 7 },
-    // teas
-    { number: 46, price: 120, descr: true, category: 8 },
-    { number: 47, price: 60, descr: true, category: 8 },
-    { number: 48, price: 60, descr: true, category: 8 },
-    { number: 49, price: 100, descr: true, category: 8 },
-    { number: 50, price: 100, descr: true, category: 8 },
-    { number: 51, price: 100, descr: true, category: 8 },
-    // cold drinks
-    { number: 52, price: 70, descr: false, category: 9 },
-    { number: 53, price: 55, descr: false, category: 9 },
-    { number: 54, price: 70, descr: false, category: 9 },
-    { number: 55, price: 75, descr: false, category: 9 },
-    { number: 56, price: 75, descr: false, category: 9 },
-    { number: 57, price: 105, descr: false, category: 9 },
-    { number: 58, price: 95, descr: false, category: 9 },
-    { number: 59, price: 85, descr: false, category: 9 },
-    { number: 60, price: 85, descr: false, category: 9 },
-    { number: 61, price: 85, descr: false, category: 9 },
-    { number: 62, price: 85, descr: false, category: 9 },
-    { number: 63, price: 35, descr: false, category: 9 },
-    { number: 64, price: 35, descr: false, category: 9 },
-    { number: 65, price: 50, descr: false, category: 9 },
-    { number: 66, price: 35, descr: false, category: 9 },
-    { number: 67, price: 50, descr: false, category: 9 },
-    // desserts
-    { number: 68, price: 105, descr: false, category: 10 },
-    { number: 69, price: 105, descr: false, category: 10 },
-    { number: 70, price: 60, descr: false, category: 10 },
-    { number: 71, price: 60, descr: false, category: 10 },
-    { number: 72, price: 105, descr: false, category: 10 },
-  ];
-
-  const categories = [
-    { number: 1 }, // breakfast
-    { number: 2 }, // toasts
-    { number: 3 }, // soups
-    { number: 4 }, // sandviches
-    { number: 5 }, // bowls
-    { number: 6 }, // salats
-    { number: 7 }, // hot drinks
-    { number: 8 }, // teas
-    { number: 9 }, // cold drinks
-    { number: 10 }, // desserts
-  ];
 
   const getCardsForArray = (array: any) => {
-    return array.map((a: any) => (
-      <PositionCard>
-        <PositionNumber>{a.number}</PositionNumber>
-        <PositionName>
-          <span>{t(`menuPositions.name${a.number}`)}</span>
-          {a.descr && <p>{t(`menuPositions.description${a.number}`)}</p>}
-        </PositionName>
-        <PositionPrice>{a.price}</PositionPrice>
-      </PositionCard>
+    return array.map((b: any) => (
+      <>
+        <Swiper
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          // cardsEffect={{ perSlideRotate: 10 }}
+          style={{
+            width: "94%",
+            padding: "3%",
+          }}
+        >
+          {b.map((a: any) => (
+            <SwiperSlide
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                background: "#ffffff",
+              }}
+            >
+              <SwiperCardImage src={a.img} />
+              <SwiperCardNumber
+                style={{
+                  position: a.img !== "" ? "absolute" : "relative",
+                  height: a.img !== "" ? "50px" : "auto",
+                  marginTop: a.img !== "" ? "0px" : "8px",
+                  color: a.img !== "" ? "black" : "gray",
+                  fontSize: a.img !== "" ? "22px" : "15px",
+                }}
+              >
+                {a.number}
+              </SwiperCardNumber>
+              <SwiperCardName>{t(`menuPositions.name${a.number}`)}</SwiperCardName>
+              {a.descr === true && (
+                <SwiperCardDescription>{t(`menuPositions.description${a.number}`)}</SwiperCardDescription>
+              )}
+              {/* <SwiperCardButton>Add to order</SwiperCardButton> */}
+              <SwiperCardPrice>{a.price} TL</SwiperCardPrice>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {b.length > 1 && (
+          <SwiperCardSwipeDescription>Пролистайте, чтобы выбрать варианты блюда</SwiperCardSwipeDescription>
+        )}
+      </>
     ));
   };
-
-  const [touchStart, setTouchStart] = React.useState(0);
-  const [touchEnd, setTouchEnd] = React.useState(0);
-
-  function handleTouchStart(e: any) {
-    setTouchStart(e.targetTouches[0].clientX);
-  }
-
-  function handleTouchMove(e: any) {
-    setTouchEnd(e.targetTouches[0].clientX);
-  }
-
-  function handleTouchEnd() {
-    if (touchStart - touchEnd > 80) {
-      if (categoryId && categoryId < 10) {
-        setCategoryId(categoryId + 1);
-        document.getElementById("tabsForPositions")?.scrollTo({
-          left:
-            (document.getElementById("tabForCategory" + (categoryId + 1).toString())?.offsetLeft ?? 0) - 80,
-        });
-      }
-    }
-
-    if (touchStart - touchEnd < -80) {
-      if (categoryId && categoryId > 1) {
-        setCategoryId(categoryId - 1);
-        document.getElementById("tabsForPositions")?.scrollTo({
-          left:
-            (document.getElementById("tabForCategory" + (categoryId - 1).toString())?.offsetLeft ?? 0) - 80,
-        });
-      }
-    }
-  }
 
   return (
     <div>
@@ -317,37 +211,9 @@ const Positions: React.FC = () => {
             ))}
           </MenuTabsScrollable>
         </MenuTabs>
-        <PositionsContainer
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
-        >
-          {getCardsForArray(positions.filter((p) => p.category === categoryId))}
+        <PositionsContainer id="tabForPositions">
+          {getCardsForArray(positions.filter((p) => p?.[0].category === categoryId))}
         </PositionsContainer>
-        {/* {positions?.map((p) => <div>{p.name}</div>)} */}
-        {/*{!!categories.length &&*/}
-        {/*    categories.map((m: any) => (*/}
-        {/*        <Block>*/}
-        {/*          <Title>*/}
-        {/*            {t(`categories.category${m.id}`)}*/}
-        {/*            <img src="/title.svg" alt="Menu"/>*/}
-        {/*          </Title>*/}
-        {/*          <TextBlock>*/}
-        {/*            <ul>*/}
-        {/*              {!!m.positions.length &&*/}
-        {/*                  m.positions.map((p: any) =>*/}
-        {/*                      (*/}
-        {/*                          <li>*/}
-        {/*                            {t(`positions.position${p.id}`)}*/}
-        {/*                            {!!p.description && <text>{p.description}</text>}*/}
-        {/*                            <strong>{p.price} TL</strong>*/}
-        {/*                          </li>*/}
-        {/*                      )*/}
-        {/*                  )}*/}
-        {/*            </ul>*/}
-        {/*          </TextBlock>*/}
-        {/*        </Block>*/}
-        {/*    ))}*/}
       </Container>
     </div>
   );
--- src/consts.ts
+++ src/consts.ts
@@ -26,3 +26,140 @@ export const LANG_MENU_ITEMS = [
   // { name: "menu.lang." + ELangs.UA, reduction: ELangs.UA },
   { name: "menu.lang." + ELangs.RU, reduction: ELangs.RU },
 ];
+
+export const positions = [
+  // 3 сырн
+  [
+    { number: 1, price: 155, descr: false, category: 1, img: "" },
+    { number: 2, price: 155, descr: false, category: 1, img: "" },
+    { number: 3, price: 155, descr: false, category: 1, img: "" },
+    { number: 4, price: 155, descr: false, category: 1, img: "" },
+    { number: 5, price: 155, descr: false, category: 1, img: "" },
+  ],
+  // 5 сырн
+  [
+    { number: 6, price: 205, descr: false, category: 1, img: "" },
+    { number: 7, price: 205, descr: false, category: 1, img: "" },
+    { number: 8, price: 205, descr: false, category: 1, img: "" },
+    { number: 9, price: 205, descr: false, category: 1, img: "" },
+    { number: 10, price: 205, descr: false, category: 1, img: "" },
+  ],
+  // яишницы
+  [{ number: 11, price: 80, descr: true, category: 1, img: "" }],
+  [{ number: 12, price: 80, descr: true, category: 1, img: "" }],
+  [{ number: 13, price: 80, descr: true, category: 1, img: "" }],
+  // блины с топингом
+  [
+    { number: 14, price: 95, descr: true, category: 1, img: "" },
+    { number: 15, price: 95, descr: true, category: 1, img: "" },
+    { number: 16, price: 95, descr: true, category: 1, img: "" },
+    { number: 17, price: 95, descr: true, category: 1, img: "" },
+    { number: 18, price: 95, descr: true, category: 1, img: "" },
+  ],
+  // блины
+  [{ number: 19, price: 135, descr: false, category: 1, img: "" }],
+  [{ number: 20, price: 135, descr: false, category: 1, img: "" }],
+  [{ number: 21, price: 155, descr: false, category: 1, img: "" }],
+  // каши рисовые
+  [
+    { number: 22, price: 110, descr: true, category: 1, img: "" },
+    { number: 23, price: 110, descr: true, category: 1, img: "" },
+    { number: 24, price: 110, descr: true, category: 1, img: "" },
+  ],
+  // овсяные каши
+  [
+    { number: 25, price: 110, descr: true, category: 1, img: "" },
+    { number: 26, price: 110, descr: true, category: 1, img: "" },
+    { number: 27, price: 110, descr: true, category: 1, img: "" },
+  ],
+  // вафли
+  [
+    { number: 28, price: 165, descr: true, category: 1, img: "" },
+    { number: 29, price: 165, descr: true, category: 1, img: "" },
+    { number: 30, price: 165, descr: true, category: 1, img: "" },
+  ],
+  // toasts
+  [{ number: 34, price: 195, descr: true, category: 2, img: "/pos34.jpg" }],
+  [{ number: 35, price: 125, descr: true, category: 2, img: "" }],
+  // soups
+  [{ number: 41, price: 95, descr: false, category: 3, img: "/pos41.jpg" }],
+  [{ number: 42, price: 95, descr: false, category: 3, img: "/pos42.jpg" }],
+  // sandviches
+  [{ number: 46, price: 165, descr: false, category: 4, img: "/pos46.jpg" }],
+  [{ number: 47, price: 165, descr: false, category: 4, img: "/pos47.jpg" }],
+  [{ number: 48, price: 145, descr: false, category: 4, img: "/pos48.jpg" }],
+  // bowls
+  [
+    { number: 51, price: 220, descr: true, category: 5, img: "/pos51.jpg" },
+    { number: 52, price: 220, descr: true, category: 5, img: "/pos51.jpg" },
+  ],
+  [
+    { number: 53, price: 220, descr: true, category: 5, img: "/pos53.jpg" },
+    { number: 54, price: 220, descr: true, category: 5, img: "/pos53.jpg" },
+  ],
+  [
+    { number: 55, price: 185, descr: true, category: 5, img: "/pos55.jpg" },
+    { number: 56, price: 185, descr: true, category: 5, img: "/pos55.jpg" },
+  ],
+  [{ number: 57, price: 165, descr: true, category: 5, img: "/pos57.jpg" }],
+  // salats
+  [{ number: 59, price: 95, descr: false, category: 6, img: "" }],
+  [{ number: 60, price: 165, descr: false, category: 6, img: "/pos60.jpg" }],
+  // hot drinks
+  [{ number: 69, price: 55, descr: false, category: 7, img: "" }],
+  [{ number: 70, price: 70, descr: false, category: 7, img: "" }],
+  [{ number: 71, price: 40, descr: false, category: 7, img: "" }],
+  [{ number: 72, price: 60, descr: false, category: 7, img: "" }],
+  [{ number: 73, price: 50, descr: false, category: 7, img: "" }],
+  [{ number: 74, price: 70, descr: false, category: 7, img: "" }],
+  [{ number: 75, price: 65, descr: false, category: 7, img: "" }],
+  [{ number: 76, price: 85, descr: false, category: 7, img: "" }],
+  [{ number: 77, price: 85, descr: false, category: 7, img: "" }],
+  [{ number: 78, price: 65, descr: false, category: 7, img: "" }],
+  [{ number: 79, price: 70, descr: false, category: 7, img: "" }],
+  [{ number: 80, price: 55, descr: false, category: 7, img: "" }],
+  [{ number: 81, price: 30, descr: false, category: 7, img: "" }],
+  // teas
+  [{ number: 61, price: 120, descr: true, category: 8, img: "" }],
+  [{ number: 62, price: 60, descr: true, category: 8, img: "" }],
+  [{ number: 63, price: 60, descr: true, category: 8, img: "" }],
+  [{ number: 64, price: 100, descr: true, category: 8, img: "" }],
+  [{ number: 65, price: 100, descr: true, category: 8, img: "" }],
+  [{ number: 66, price: 100, descr: true, category: 8, img: "" }],
+  // cold drinks
+  [{ number: 86, price: 70, descr: false, category: 9, img: "" }],
+  [{ number: 87, price: 55, descr: false, category: 9, img: "" }],
+  [{ number: 88, price: 70, descr: false, category: 9, img: "" }],
+  [{ number: 89, price: 75, descr: false, category: 9, img: "" }],
+  [{ number: 90, price: 75, descr: false, category: 9, img: "" }],
+  [{ number: 91, price: 105, descr: false, category: 9, img: "" }],
+  [{ number: 92, price: 95, descr: false, category: 9, img: "" }],
+  [{ number: 93, price: 85, descr: false, category: 9, img: "" }],
+  [{ number: 94, price: 85, descr: false, category: 9, img: "" }],
+  [{ number: 95, price: 35, descr: false, category: 9, img: "" }],
+  [{ number: 96, price: 35, descr: false, category: 9, img: "" }],
+  [{ number: 97, price: 50, descr: false, category: 9, img: "" }],
+  [{ number: 98, price: 35, descr: false, category: 9, img: "" }],
+  [{ number: 99, price: 35, descr: false, category: 9, img: "" }],
+  [{ number: 100, price: 50, descr: false, category: 9, img: "" }],
+  // desserts
+  [{ number: 105, price: 105, descr: false, category: 10, img: "" }],
+  [{ number: 106, price: 105, descr: false, category: 10, img: "" }],
+  [{ number: 107, price: 60, descr: false, category: 10, img: "" }],
+  [{ number: 108, price: 60, descr: false, category: 10, img: "" }],
+  [{ number: 109, price: 105, descr: false, category: 10, img: "" }],
+];
+
+export const categories = [
+  { number: 1 }, // breakfast
+  { number: 2 }, // toasts
+  { number: 3 }, // soups
+  { number: 4 }, // sandviches
+  { number: 5 }, // bowls
+  { number: 6 }, // salats
+  { number: 7 }, // hot drinks
+  { number: 9 }, // cold drinks
+  { number: 8 }, // teas
+  { number: 10 }, // desserts
+  // { number: 11 }, // add
+];
